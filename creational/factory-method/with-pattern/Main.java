8c8ea5134a2116e39ad0a8d81799781e79673635
Yuri Fernandes <yuri_fernandes2000@hotmail.com>
2026-02-04 20:17:10 -0300

    Teste da pattern factory

--- creational/factory-method/with-pattern/Main.java
+++ creational/factory-method/with-pattern/Main.java
@@ -1,6 +1,8 @@
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+        NotificationService.send(NotificationType.EMAIL, "Welcome!");
+        NotificationService.send(NotificationType.SMS, "Your code is 1234");
+        NotificationService.send(NotificationType.PUSH, "New message received");
     }
 }
